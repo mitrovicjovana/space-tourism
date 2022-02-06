8a26a5a399037ed85b0f3f77913909ff2f79bc95
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-02-06 22:34:17 +0100

    Add assets

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+// Icons
+export { default as iconClose } from './icons/icon-close.svg'
+export { default as iconHamburger } from './icons/icon-hamburger.svg'
+export { default as logo } from './icons/logo.svg'
+
+// Home
+export { default as homeBgMobile } from './home/background-home-mobile.jbg'
+export { default as homeBgTablet } from './home/background-home-tablet.jbg'
+export { default as homeBgDesktop } from './home/background-home-desktop.jbg'
+
+// Destination
+
+// Crew
+
+// Technology
