ffa549a42a8ddd9e91c977b3301b1a3be8d8f110
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-02-07 10:41:39 +0100

    Add JSX for home page

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,13 +3,22 @@ export { default as iconClose } from './icons/icon-close.svg'
 export { default as iconHamburger } from './icons/icon-hamburger.svg'
 export { default as logo } from './icons/logo.svg'
 
-// Home
-export { default as homeBgMobile } from './home/background-home-mobile.jbg'
-export { default as homeBgTablet } from './home/background-home-tablet.jbg'
-export { default as homeBgDesktop } from './home/background-home-desktop.jbg'
-
 // Destination
+export { default as europaImage } from './images/crew/image-europa.png'
+export { default as marsImage } from './images/crew/image-mars.png'
+export { default as moonImage } from './images/crew/image-moon.png'
+export { default as titanImage } from './images/crew/image-titan.png'
 
 // Crew
+export { default as anoushehAnsariImage } from './images/crew/image-anousheh-ansari.png'
+export { default as douglasHurleyImage } from './images/crew/image-douglas-hurley.png'
+export { default as markShuttleworthImage } from './images/crew/image-mark-shuttleworth.png'
+export { default as victorGloverImage } from './images/crew/image-victor-glover.png'
 
 // Technology
+export { default as lauchVehicleLandscapeImage } from './images/crew/image-lauch-vehicle-landscape.png'
+export { default as lauchVehiclePortraitImage } from './images/crew/image-lauch-vehicle-portrait.png'
+export { default as spaceCapsuleLandscapeImage } from './images/crew/image-space-capsule-landscape.png'
+export { default as spaceCapsulePortraitImage } from './images/crew/image-space-capsule-portrait.png'
+export { default as spaceportLandscapeImage } from './images/crew/image-spaceport-landscape.png'
+export { default as spaceportPortraitImage } from './images/crew/image-spaceport-portrait.png'
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,20 @@
+import './home.scss'
+
 const Home = () => {
-   return <div style={{ width: '100%', height: '100vh', backgroundColor: 'dodgerblue' }}>home</div>
+   return (
+      <section className="home">
+         <div className="home__info">
+            <h5 className="home__heading--h5">So, you want to travel to</h5>
+            <h1 className="home__heading--h1">Space</h1>
+            <p className="home__text">
+               Let’s face it; if you want to go to space, you might as well genuinely go to outer
+               space and not hover kind of on the edge of it. Well sit back, and relax because we’ll
+               give you a truly out of this world experience!
+            </p>
+         </div>
+         <button>Explore</button>
+      </section>
+   )
 }
 
 export default Home
