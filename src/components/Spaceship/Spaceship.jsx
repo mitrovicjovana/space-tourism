8c0a9a0d2294a00ccb3f320c2058147c69c25d94
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-02-09 22:42:46 +0100

    Add technology page

--- src/components/Spaceship/Spaceship.jsx
+++ src/components/Spaceship/Spaceship.jsx
@@ -1,30 +1,14 @@
 import './spaceship.scss'
 
-import { lauchVehiclePortraitImage, lauchVehicleLandscapeImage } from '../../assets'
-
-const Spaceship = () => {
-   const {
-      name,
-      images: { portrait, landscape },
-      description,
-   } = {
-      name: 'Launch vehicle',
-      images: {
-         portrait: lauchVehiclePortraitImage,
-         landscape: lauchVehicleLandscapeImage,
-      },
-      description:
-         "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
-   }
-
+const Spaceship = ({ children, name, images: { portrait, landscape }, description, isActive }) => {
    return (
-      <div className="spaceship">
+      <div className={`spaceship${isActive === true ? ' active' : ''}`}>
          <picture className="spaceship__image">
             <source media="(min-width:1024px)" srcSet={portrait} />
             <img className="spaceship__image" src={landscape} alt={name} />
          </picture>
          <div className="wrapper">
-            <div className="spaceship__numbers">1 2 3</div>
+            <div className="spaceship__numbers">{children}</div>
             <div className="spaceship__text">
                <h6 className="spaceship__terminology nav-item">The terminology...</h6>
                <h3 className="spaceship__name">{name}</h3>
--- src/pages/Technology/Technology.jsx
+++ src/pages/Technology/Technology.jsx
@@ -1,12 +1,44 @@
+import { useState } from 'react'
 import { Spaceship } from '../../components'
 import './technology.scss'
 
+import { technology } from '../../assets/data'
+
 const Technology = () => {
+   const [activeSlide, setActiveSlide] = useState(0)
+
+   const changeActiveSlide = active => {
+      setActiveSlide(active)
+   }
+
    return (
       <section className="technology">
          <div style={{ minHeight: '100vh' }}>
-            <div className="wrapper"></div>
-            <Spaceship />
+            <div className="wrapper">
+               <h5 className="page-heading heading">
+                  <span>03</span>
+                  Space launch 101
+               </h5>
+            </div>
+            {technology.map(({ name, images, description, isActive }, index) => (
+               <Spaceship
+                  key={index}
+                  name={name}
+                  images={images}
+                  description={description}
+                  isActive={index === activeSlide}
+               >
+                  {technology.map((_, order) => (
+                     <button
+                        key={order}
+                        className={`technology__number${order === activeSlide ? ' active' : ''}`}
+                        onClick={() => changeActiveSlide(order)}
+                     >
+                        {order + 1}
+                     </button>
+                  ))}
+               </Spaceship>
+            ))}
          </div>
       </section>
    )
