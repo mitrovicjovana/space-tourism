338e49299ae3927eaa16f06511197cce7b6722a5
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-02-09 18:10:27 +0100

    Add crew page

--- src/components/CrewMember/CrewMember.jsx
+++ src/components/CrewMember/CrewMember.jsx
@@ -1,25 +1,15 @@
 import './crew-member.scss'
-import { douglasHurleyImage } from '../../assets'
 
-const CrewMember = () => {
-   const humanoid = {
-      name: 'Douglas Hurley',
-      image: douglasHurleyImage,
-      role: 'Commander',
-      bio: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
-   }
-
-   const { name, image, role, bio } = humanoid
-
+const CrewMember = ({ children, name, image, role, bio, isActive }) => {
    return (
-      <div className="member">
+      <div className={`member${isActive === true ? ' active' : ''}`}>
          <div className="member__details">
             <div className="member__info">
                <h4 className="member__role">{role}</h4>
                <h3 className="member__name">{name}</h3>
                <p className="member__bio">{bio}</p>
             </div>
-            <div className="member__dots">DOTS</div>
+            <div className="member__dots">{children}</div>
          </div>
          <div className="member__image-container">
             <img src={image} alt={name} className="member__image" />
--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react'
 import { CrewMember } from '../../components'
 import './crew.scss'
 
+import { crew } from '../../assets/data'
+
 const Crew = () => {
+   const [activeSlide, setActiveSlide] = useState(0)
+
+   useEffect(() => {
+      const time = setInterval(() => {
+         activeSlide === crew.length - 1
+            ? setActiveSlide(0)
+            : setActiveSlide(prevSlide => prevSlide + 1)
+      }, 7000)
+
+      return () => clearInterval(time)
+   }, [activeSlide])
+
+   const changeActiveSlide = active => {
+      setActiveSlide(active)
+   }
+
    return (
       <section className="crew">
          <div className="wrapper">
@@ -9,7 +28,26 @@ const Crew = () => {
                <span>02</span>
                Meet your crew
             </h5>
-            <CrewMember />
+            {crew.map(({ name, image, role, bio }, index) => (
+               <CrewMember
+                  key={index}
+                  name={name}
+                  image={image}
+                  role={role}
+                  bio={bio}
+                  isActive={index === activeSlide}
+               >
+                  {crew.map((_, order) => (
+                     <button
+                        key={order}
+                        className={`dot${order === activeSlide ? ' active' : ''}`}
+                        onClick={() => {
+                           changeActiveSlide(order)
+                        }}
+                     />
+                  ))}
+               </CrewMember>
+            ))}
          </div>
       </section>
    )
